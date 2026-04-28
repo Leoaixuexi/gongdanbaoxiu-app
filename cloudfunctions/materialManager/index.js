55b8c6aa4b3c9f153a77266f42478d792c70982c
Leoaixuexi <leoxuebiancheng0522@gmail.com>
2026-04-28 10:32:16 +0800

    Revert "feat(material): + getStockOutRequest action（详情，含权限过滤）"
    This reverts commit d27441ff62b6e64a5737966e1a95bfe6f079a28e.

--- cloudfunctions/materialManager/index.js
+++ cloudfunctions/materialManager/index.js
@@ -29,7 +29,6 @@ const ROUTES = {
   rejectStockOutRequest: request.rejectStockOutRequest,
   cancelStockOutRequest: request.cancelStockOutRequest,
   listStockOutRequests: request.listStockOutRequests,
-  getStockOutRequest: request.getStockOutRequest,
   // Seed
   seedTestData: seed.seedTestData,
 };
